e698fdf1d522ed1a138d881f4f1a556b1589d44f
Bart Massey <bart@cs.pdx.edu>
2018-05-30 00:48:22 -0700

    got game working right

--- src/n15_server.rs
+++ src/n15_server.rs
@@ -25,14 +25,56 @@ fn get_client() -> TcpStream {
     }
 }
 
+fn choose(s: &HashSet<u64>, n: u64) -> Vec<HashSet<u64>> {
+    if n == 0 || s.len() < n as usize {
+        return Vec::new();
+    }
+    if s.len() == n as usize {
+        return vec![s.clone()];
+    }
+    let mut result: Vec<HashSet<u64>> = Vec::new();
+    for e in s {
+        let mut t = s.clone();
+        t.remove(e);
+        result.extend(choose(&t, n));
+        let v: Vec<HashSet<u64>> = choose(&t, n - 1)
+            .into_iter()
+            .map(|mut w| {
+                w.insert(*e);
+                w
+            })
+            .collect();
+        result.extend(v);
+    }
+    result
+}
+
+struct PlayerState(HashSet<u64>);
+
+impl PlayerState {
+    fn new() -> PlayerState {
+        PlayerState(HashSet::new())
+    }
+
+    fn insert(&mut self, e: u64) {
+        assert!(self.0.insert(e));
+    }
+
+    fn won(&self) -> Option<HashSet<u64>> {
+        choose(&self.0, 3).into_iter().find(|s| s.iter().sum::<u64>() == 15)
+    }
+}
+
 fn game_loop<T: BufRead, U: Write>(mut reader: T, mut writer: U) -> Result<(), std::io::Error> {
     let mut unused = HashSet::new();
     for i in 1..=9 {
         unused.insert(i);
     }
-    let mut my_total = 0;
-    let mut your_total = 0;
+    let mut you = PlayerState::new();
+    let mut me = PlayerState::new();
     loop {
+        writeln!(writer, "me: {:?}", me.0)?;
+        writeln!(writer, "you: {:?}", you.0)?;
         writeln!(writer, "available: {:?}", unused)?;
         write!(writer, "move: ")?;
         writer.flush()?;
@@ -50,13 +92,14 @@ fn game_loop<T: BufRead, U: Write>(mut reader: T, mut writer: U) -> Result<(), s
             writeln!(writer, "unavailable choice try again")?;
             continue;
         }
-        your_total += n;
-        if your_total == 15 {
+        you.insert(n);
+        if let Some(win) = you.won() {
+            writeln!(writer, "{:?}", win)?;
             writeln!(writer, "you win")?;
             return Ok(());
         }
-        if your_total > 15 || unused.is_empty() {
-            writeln!(writer, "you lose")?;
+        if unused.is_empty() {
+            writeln!(writer, "draw")?;
             return Ok(());
         }
         let choice;
@@ -65,22 +108,26 @@ fn game_loop<T: BufRead, U: Write>(mut reader: T, mut writer: U) -> Result<(), s
             let index = random::<usize>() % choicevec.len();
             choice = *choicevec[index];
         }
+        writeln!(writer, "I choose {}", choice)?;
         unused.remove(&choice);
-        my_total += choice;
-        if my_total == 15 {
+        me.insert(choice);
+        if let Some(win) = me.won() {
+            writeln!(writer, "{:?}", win)?;
             writeln!(writer, "I win")?;
             return Ok(());
         }
-        if my_total > 15 || unused.is_empty() {
-            writeln!(writer, "I lose")?;
+        if unused.is_empty() {
+            writeln!(writer, "draw")?;
             return Ok(());
         }
     }
 }
 
 fn main() {
-    let reader = get_client();
-    let mut writer = reader.try_clone().unwrap();
-    writeln!(writer, "n15 v0.0.0.1").unwrap();
-    game_loop(std::io::BufReader::new(reader), writer).unwrap();
+    loop {
+        let reader = get_client();
+        let mut writer = reader.try_clone().unwrap();
+        writeln!(writer, "n15 v0.0.0.1").unwrap();
+        game_loop(std::io::BufReader::new(reader), writer).unwrap();
+    }
 }
